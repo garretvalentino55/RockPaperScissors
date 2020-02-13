9e598982a028b24f374bd0746972752002dc365d
Garret Valentino <garret.valentino@gmail.com>
2020-02-13 10:49:43 -0600

    i have done a fair bit of work i have added things to the human class and i think i am almost done there once i have that finished then i will be moving on to the actual game logic and that should allow me to finish the gamethe goal is to have it done today before i leave

--- RockPaperScissors/Human.cs
+++ RockPaperScissors/Human.cs
@@ -17,27 +17,79 @@ namespace RockPaperScissors
 
         }
 
-            //member methods 
+        //member methods 
 
-        public void Choices() 
+        public void Choice(int selection)
         {
-            gestureChoiceArray[0] = "Rock";
-            gestureChoiceArray[1] = "Paper";
-            gestureChoiceArray[2] = "Scissors";
-            gestureChoiceArray[3] = "Lizard";
-            gestureChoiceArray[4] = "Spock";
+
+            if (selection == 1)
+            {
+                GestureChoice = GestureOptions[0];
+            }
+            if (selection == 2)
+            {
+                GestureChoice = GestureOptions[1];
+            }
+            if (selection == 3)
+            {
+                GestureChoice = GestureOptions[2];
+            }
+            if (selection == 4)
+            {
+                GestureChoice = GestureOptions[3];
+            }
+            if (selection == 5)
+            {
+                GestureChoice = GestureOptions[4];
+            }
         }
-        public override int ChooseGesture()
+        public override int ChooseGesture(Player eachPlayer)
         {
-            
+            int gestureChoice = 0;
+            Console.WriteLine("Your Choices are: ");
+            foreach (Gestures gesture in GestureOptions)
+            {
+                Console.WriteLine(gesture.name);
+
+            }
+            string playerChoice = Console.ReadLine();
+
+            bool isValid = false;
+            int numberOfWrong = 0;
+            do
+
+            {
+                switch (playerChoice)
+                {
+                    case "Rock":
+                        gestureChoice = 0;
+                        break;
+                    case "Paper":
+                        gestureChoice = 1;
+                        break;
+                    case "Scissors":
+                        gestureChoice = 2;
+                        break;
+                    case "Lizard":
+                        gestureChoice = 3;
+                        break;
+                    case "Spock":
+                        gestureChoice = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Please choose a number from the list");
+                        numberOfWrong++;
+                        break;
+                }
+                Console.ReadLine();
+            } while (isValid == false && numberOfWrong < 5);
+
+
+
         }
 
-        public override void ChooseGesture(Player eachPlayer)
-        {
-        
-        }
+    }   
 
-        Console.WriteLine("choose your gesture");
-        gestures=Console.Readline();
-    }
+    
 }
+
